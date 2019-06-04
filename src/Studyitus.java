f1eca79bb0e04f8f4e7f421ebe7b84630568d90a
Joban <Joban@LAPTOP-6GDEOP3F>
2019-06-04 16:13:13 -0400

    R1 Commit

---
+++ src/Studyitus.java
@@ -0,0 +1,19 @@
+// Jobanpreet Singh - C0748529
+public class Studyitus {
+
+	public static void main(String[] args) {
+		// Quiz 5 & 6
+
+		//Requirement 1
+		String output = studying("Shivam");
+		System.out.println(output);
+		
+		
+	}
+	
+	public static String studying(String name) {
+		
+		return name + " is studying";
+	}
+
+}
